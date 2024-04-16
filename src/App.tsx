52a7cfed04df1e1425b1960ec786182af4c627b3
Alfonso Ramos <alfonsoramos.1803@gmail.com>
2024-04-16 12:30:17 -0700

    [ADD] Agregado useReducer y eliminado el customHook

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,44 @@
+import { useReducer, useEffect } from 'react';
+
 import Header from "./components/Header"
 import Guitar from "./components/Guitar"
 
-import { useCart } from "./hooks/useCart"
+import { cartReducer, initialState } from "./reducers/cartReducer"
 
 function App() {
 
-  const { data, cart, addToCart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal} = useCart()
+
+  const [state, dispatch] = useReducer(cartReducer, initialState)
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart])
+
   return (
     <>
-    <Header 
-      cart={cart}
-      removeFromCart={removeFromCart}
-      increaseQuantity={increaseQuantity}
-      decreaseQuantity={decreaseQuantity}
-      clearCart={clearCart}
-      isEmpty={isEmpty}
-      cartTotal={cartTotal}
-    />
+      <Header
+        cart={state.cart}
+        dispatch={dispatch}
+      />
       <main className="container-xl mt-5">
-          <h2 className="text-center">Nuestra Colección</h2>
+        <h2 className="text-center">Nuestra Colección</h2>
 
-          <div className="row mt-5">
-            {data.map((guitar) => (
-              <Guitar
-                key={guitar.id}
-                guitar={guitar}
-                addToCart={addToCart}
-              />
-            ))}
-          </div>
+        <div className="row mt-5">
+          {state.data.map((guitar) => (
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              dispatch={dispatch}
+            />
+          ))}
+        </div>
       </main>
 
 
       <footer className="bg-dark mt-5 py-5">
-          <div className="container-xl">
-              <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
-          </div>
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
       </footer>
     </>
   )
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,20 @@
-import type { CartItem, Guitar } from "../types"
+import { useMemo, Dispatch } from "react"
+import type { CartItem } from "../types"
+import { CartActions } from '../reducers/cartReducer';
+
 type HeaderProps = {
     cart: CartItem[],
-    removeFromCart: (id : Guitar['id']) => void,
-    increaseQuantity: (id : Guitar['id']) => void,
-    decreaseQuantity: (id : Guitar['id']) => void,
-    clearCart: () => void,
-    isEmpty: boolean,
-    cartTotal: number
+    dispatch: Dispatch<CartActions>
 }
 
-export default function Header({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal } : HeaderProps) {
+export default function Header({ cart, dispatch } : HeaderProps) {
+
+    // State derivado
+    // Codigo que se ejecuta cada que el cart cambie
+    const isEmpty = useMemo( () => cart.length === 0, [cart] )
+    const cartTotal = useMemo(() => cart.reduce((total, item)  => total + (item.quantity * item.price), 0), [cart])
+
+    useMemo
     return (
         <header className="py-5 header">
             <div className="container-xl">
@@ -55,7 +60,7 @@ export default function Header({ cart, removeFromCart, increaseQuantity, decreas
                                                         <button
                                                             type="button"
                                                             className="btn btn-dark"
-                                                            onClick={() => decreaseQuantity(guitar.id)}
+                                                            onClick={() => dispatch({type: 'decrease-quantity', payload: {id: guitar.id}})}
                                                         >
                                                             -
                                                         </button>
@@ -63,7 +68,7 @@ export default function Header({ cart, removeFromCart, increaseQuantity, decreas
                                                         <button
                                                             type="button"
                                                             className="btn btn-dark"
-                                                            onClick={() => increaseQuantity(guitar.id)}
+                                                            onClick={() => dispatch({type: 'increase-quantity', payload: {id: guitar.id}})}
                                                         >
                                                             +
                                                         </button>
@@ -72,7 +77,7 @@ export default function Header({ cart, removeFromCart, increaseQuantity, decreas
                                                         <button
                                                             className="btn btn-danger"
                                                             type="button"
-                                                            onClick={() => removeFromCart(guitar.id)}
+                                                            onClick={() => dispatch({type: "remove-from-cart", payload: {id: guitar.id}})}
                                                         >
                                                             X
                                                         </button>
@@ -86,7 +91,7 @@ export default function Header({ cart, removeFromCart, increaseQuantity, decreas
                                 )}
                                 <button 
                                     className="btn btn-dark w-100 mt-3 p-2"
-                                    onClick={clearCart}
+                                    onClick={() => dispatch({type: 'clear-cart'})}
                                 >Vaciar Carrito</button>
                             </div>
                         </div>
